3c1a58136abbb0fa1960e941d3d71132c6020752
ewecke <edgargandolfi@gmail.com>
2020-08-18 15:28:43 +0200

    add-key-id-to-data

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import Avatar from './Avatar';
 function App() {
   
   const data =[
-    {size:"s",type:"square", xy:"30x30"},
-    {size:"s",type:"rounded", xy:"30x30"},
-    {size:"s",type:"circle", xy:"30x30"},
-    {size:"m",type:"square", xy:"60x60"},
-    {size:"m",type:"rounded", xy:"60x60"},
-    {size:"m",type:"circle", xy:"60x60"},
-    {size:"l",type:"square", xy:"120X120"},
-    {size:"l",type:"rounded", xy:"120X120"},
-    {size:"l",type:"circle", xy:"120X120"},
-    {size:"xl",type:"square", xy:"200X200"},
-    {size:"xl",type:"rounded", xy:"200X200"},
-    {size:"xl",type:"circle", xy:"200X200"},
+    {id:1,size:"s",type:"square", xy:"30x30"},
+    {id:2,size:"s",type:"rounded", xy:"30x30"},
+    {id:3,size:"s",type:"circle", xy:"30x30"},
+    {id:4,size:"m",type:"square", xy:"60x60"},
+    {id:5,size:"m",type:"rounded", xy:"60x60"},
+    {id:6,size:"m",type:"circle", xy:"60x60"},
+    {id:7,size:"l",type:"square", xy:"120X120"},
+    {id:8,size:"l",type:"rounded", xy:"120X120"},
+    {id:9,size:"l",type:"circle", xy:"120X120"},
+    {id:10,size:"xl",type:"square", xy:"200X200"},
+    {id:11,size:"xl",type:"rounded", xy:"200X200"},
+    {id:12,size:"xl",type:"circle", xy:"200X200"},
   ]
 
   return (
@@ -24,7 +24,7 @@ function App() {
       <div className="container"> 
       {data.map(item=>{
         return (
-          <Avatar size={item.size} type={item.type} xy={item.xy} />
+          <Avatar key={item.id} size={item.size} type={item.type} xy={item.xy} />
         )
       })}
 
@@ -32,6 +32,5 @@ function App() {
   
   );
 }
-
 
 export default App;
